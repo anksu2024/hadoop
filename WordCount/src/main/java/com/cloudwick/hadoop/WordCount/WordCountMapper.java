2d1ba6907f5caab54990bca8921fefac45f4404b
Ankit Sarraf <sarrafankit@hotmail.com>
2015-06-03 11:24:14 -0700

    Initial Commit - WordCount

---
+++ WordCount/src/main/java/com/cloudwick/hadoop/WordCount/WordCountMapper.java
@@ -0,0 +1,23 @@
+package com.cloudwick.hadoop.WordCount;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+	@Override
+	protected void map(LongWritable key, Text value,
+			Mapper<LongWritable, Text, Text, IntWritable>.Context context)
+			throws IOException, InterruptedException {
+		String line = value.toString();
+		String[] lineArray = line.split("\\s+");
+
+		IntWritable outValue = new IntWritable(1);
+		for (String array : lineArray) {
+			context.write(new Text(array.trim()), outValue);
+		}
+	}
+}
---
+++ WordCount/src/main/java/com/cloudwick/hadoop/WordCount/WordCountReducer.java
@@ -0,0 +1,28 @@
+package com.cloudwick.hadoop.WordCount;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class WordCountReducer extends
+		Reducer<Text, IntWritable, Text, IntWritable> {
+	@Override
+	protected void reduce(Text key, Iterable<IntWritable> counts,
+			Reducer<Text, IntWritable, Text, IntWritable>.Context context)
+			throws IOException, InterruptedException {
+
+		int counter = 0;
+
+		Iterator<IntWritable> iterator = counts.iterator();
+		while (iterator.hasNext()) {
+			counter += 1;
+			iterator.next();
+		}
+
+		System.out.println(key + ":" + counter);
+		context.write(key, new IntWritable(counter));
+	}
+}
