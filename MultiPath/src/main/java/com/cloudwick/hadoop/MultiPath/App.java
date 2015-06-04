9391886ce22b6477e89fdfd7f88ff08ccde05c18
Ankit Sarraf <sarrafankit@hotmail.com>
2015-06-04 09:52:00 -0700

    MultiPath - Initial Commit

---
+++ MultiPath/src/main/java/com/cloudwick/hadoop/MultiPath/App.java
@@ -0,0 +1,13 @@
+package com.cloudwick.hadoop.MultiPath;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
